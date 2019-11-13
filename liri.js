993ac540227986349c232d37cbaa937c7ad8082a
cvevera <cvevera@gmail.com>
2019-11-12 21:30:58 -0500

    added requires for all packageds to liri.js and layed out switch statment and functions for app

--- liri.js
+++ liri.js
@@ -1,5 +1,22 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
+var spotifyAPI = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+var axios = require("axios");
+var fs = require("fs");
+
+var command = process.argv[2];
+var parameter = process.argv[3];
+
+switch 
+//TO DO assign the various functions to switch cases here
+
+function concertThis
+
+function spotifyThisSong
+
+function movieThis
+
+function doWhatItSays
