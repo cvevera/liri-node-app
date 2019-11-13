b92719009b24c5361c50297161b7b197f262d99d
cvevera <cvevera@gmail.com>
2019-11-13 12:51:01 -0500

    tested switch case functionality with placeholder functions

--- liri.js
+++ liri.js
@@ -2,21 +2,43 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var spotifyAPI = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var axios = require("axios");
 var fs = require("fs");
 
 var command = process.argv[2];
-var parameter = process.argv[3];
+var parameter = process.argv.slice(3).join(" ");
 
-switch 
-//TO DO assign the various functions to switch cases here
+// Determines the command selected...
+// Based on the command we run the appropriate function
+switch (command) {
+    case "concert-this":
+        concertThis(parameter);
+        break;
+    case "spotify-this-song":
+        spotifyThisSong(parameter);
+        break;
+    case "movie-this":
+        movieThis(parameter);
+        break;
+    case "do-what-it-says":
+        doWhatItSays(parameter);
+        break;
+};
 
-function concertThis
+function concertThis() {
+    console.log("Concert this " + parameter)
+};
 
-function spotifyThisSong
+function spotifyThisSong() {
+    console.log("Spotify this " + parameter)
+};
 
-function movieThis
+function movieThis() {
+    console.log("Movie this " + parameter)
+};
 
-function doWhatItSays
+function doWhatItSays() {
+    console.log("Do what " + parameter + " says!")
+};
