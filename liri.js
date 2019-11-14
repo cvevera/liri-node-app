7df4bc51f25baf28b367066093a2d7b2d55a798e
cvevera <cvevera@gmail.com>
2019-11-14 00:10:27 -0500

    added functionality for concert-this, added code (not functioning) for spotify-this)

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotifyAPI = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
+var SpotifyAPI = require("node-spotify-api");
+var spotify = new SpotifyAPI(keys.spotify);
 var moment = require('moment');
 var axios = require("axios");
 var fs = require("fs");
@@ -27,43 +27,63 @@ switch (command) {
         break;
 };
 
-// function concertThis() {
-//     axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp")
-//     .then(function(response){
-//     console.log(response.data.venue.name)})
-
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-// };
-
-function spotifyThisSong() {
-    console.log("Spotify this " + parameter)
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp")
+    .then(function(response){
+        for(var i = 0; i < 5; i++){
+            var concertData = [
+                "Venue: " + response.data[i].venue.name,
+                "Location: " + response.data[i].venue.city,
+                "Event Date: " + response.data[i].datetime,
+                "-------------------------------------",
+            ].join("\n\n");
+    console.log(concertData)}
+})
+    .catch(function (error) {
+        console.log(error);
+    });
 };
+
+// function spotifyThisSong() {
+//     spotify.search({ type: 'track', query: parameter }, function (response) {
+//         for (var i = 0; i < 5; i++) {
+//             var spotifyData = [
+//                 "Artist: " + response.tracks.items[i].artists[0].name,
+//                 "Song Name: " + response.tracks.items[i].name,
+//                 "Album Name: " + response.tracks.items[i].album.name,
+//                 "Preview Link: " + response.tracks.items[i].preview_url,
+//             ].join("\n\n");
+//             console.log(spotifyData);
+//         }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// };
 
 function movieThis() {
     // This makes the default parameter "Mr. Nobody"
     if (!parameter) {
         parameter = "Mr. Nobody";
-      }
+    }
     axios.get("https://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy")
-    .then(function(response) {
-        var movieData = [
-            "Movie Title: " + response.data.Title,
-            "Year of Release:"  + response.data.Year,
-            "IMDB Rating: " + response.data.imdbRating,
-            "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value,
-            "Country Produced: " + response.data.Country,
-            "Language: " + response.data.Language,
-            "Plot: " + response.data.Plot,
-            "Actors/Actresses: " + response.data.Actors].join("\n\n");
-    console.log(movieData);
-})
-.catch(function (error) {
-console.log(error);
-});
+        .then(function (response) {
+            var movieData = [
+                "Movie Title: " + response.data.Title,
+                "Year of Release:" + response.data.Year,
+                "IMDB Rating: " + response.data.imdbRating,
+                "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value,
+                "Country Produced: " + response.data.Country,
+                "Language: " + response.data.Language,
+                "Plot: " + response.data.Plot,
+                "Actors/Actresses: " + response.data.Actors].join("\n\n");
+            console.log(movieData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
 function doWhatItSays() {
-    console.log("Do what " + parameter + " says!")
+    
 };
