165038a69a4f1091b2228c8a55f3a1db1b3e84e0
cvevera <cvevera@gmail.com>
2019-11-14 14:57:30 -0500

    Added DoWhatItSays functionality and functionality to write all commands to the log.txt file

--- liri.js
+++ liri.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+
 var SpotifyAPI = require("node-spotify-api");
 var spotify = new SpotifyAPI(keys.spotify);
+
 var moment = require('moment');
+moment().format("MM/DD/YYYY");
+
 var axios = require("axios");
+
 var fs = require("fs");
 
 var command = process.argv[2];
@@ -29,48 +34,53 @@ switch (command) {
 
 function concertThis() {
     axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp")
-    .then(function(response){
-        for(var i = 0; i < 5; i++){
-            var concertData = [
-                "Venue: " + response.data[i].venue.name,
-                "Location: " + response.data[i].venue.city,
-                "Event Date: " + response.data[i].datetime,
-                "-------------------------------------",
-            ].join("\n\n");
-    console.log(concertData)}
-})
-    .catch(function (error) {
-        console.log(error);
-    });
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+                var timeFormat = moment(response.data[i].datetime).format("MM/DD/YYYY");
+
+                var concertData = [
+                    "-------------------------------------",
+                    "Venue: " + response.data[i].venue.name,
+                    "Location: " + response.data[i].venue.city,
+                    "Event Date: " + timeFormat,
+                    " ",
+                ].join("\n\n");
+                console.log(concertData)
+                fs.appendFile("log.txt", concertData, function(err) {});
+        }})
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
 function spotifyThisSong() {
     spotify.search({ type: 'track', query: parameter })
-    .then (function (response) {
-        for (var i = 0; i < 5; i++) {
-            var spotifyData = [
-                "----------------------------------------",
-                "Artist: " + response.tracks.items[i].artists[0].name,
-                "Song Name: " + response.tracks.items[i].name,
-                "Album Name: " + response.tracks.items[i].album.name,
-                "Preview Link: " + response.tracks.items[i].preview_url,
-                " ",
-            ].join("\n\n");
-            console.log(spotifyData);
-        }
-     
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+                var spotifyData = [
+                    "----------------------------------------",
+                    "Artist: " + response.tracks.items[i].artists[0].name,
+                    "Song Name: " + response.tracks.items[i].name,
+                    "Preview Link: " + response.tracks.items[i].preview_url,
+                    "Album Name: " + response.tracks.items[i].album.name,
+                    " ",
+                ].join("\n\n");
+                console.log(spotifyData);
+                fs.appendFile("log.txt", spotifyData, function(err) {});
+            }
+
         })
         .catch(function (error) {
             console.log(error);
         });
-//     spotify
-//   .search({ type: 'track', query: parameter })
-//   .then(function(response) {
-//     console.log(response.tracks.items[0]);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+    //     spotify
+    //   .search({ type: 'track', query: parameter })
+    //   .then(function(response) {
+    //     console.log(response.tracks.items[0]);
+    //   })
+    //   .catch(function(err) {
+    //     console.log(err);
+    //   });
 };
 
 function movieThis() {
@@ -89,16 +99,82 @@ function movieThis() {
                 "Country Produced: " + response.data.Country,
                 "Language: " + response.data.Language,
                 "Plot: " + response.data.Plot,
-                "Actors/Actresses: " + response.data.Actors,
+                "Actors: " + response.data.Actors,
                 " ",
             ].join("\n\n");
             console.log(movieData);
+            fs.appendFile("log.txt", movieData, function(err) {});
         })
         .catch(function (error) {
             console.log(error);
         });
 };
 
+// Each function is present within doWhatItSays, so any command and parameter written in the random.txt file can work. (Not efficient but it works!)
 function doWhatItSays() {
-    
+    fs.readFile("random.txt", "utf8", function (err, result) {
+        var fileCommand = result.split(",");
+
+        if (fileCommand[0] === "spotify-this-song") {
+            spotify.search({ type: 'track', query: fileCommand[1] })
+                .then(function (response) {
+                    for (var i = 0; i < 5; i++) {
+                        var spotifyData = [
+                            "----------------------------------------",
+                            "Artist: " + response.tracks.items[i].artists[0].name,
+                            "Song Name: " + response.tracks.items[i].name,
+                            "Preview Link: " + response.tracks.items[i].preview_url,
+                            "Album Name: " + response.tracks.items[i].album.name,
+                            " ",
+                        ].join("\n\n");
+                        console.log(spotifyData);
+                    }
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+        if (fileCommand[0] === "concert-this") {
+            axios.get("https://rest.bandsintown.com/artists/" + fileCommand[1] + "/events?app_id=codingbootcamp")
+                .then(function (response) {
+                    for (var i = 0; i < 5; i++) {
+                        var timeFormat = moment(response.data[i].datetime).format("MM/DD/YYYY");
+
+                        var concertData = [
+                            "Venue: " + response.data[i].venue.name,
+                            "Location: " + response.data[i].venue.city,
+                            "Event Date: " + timeFormat,
+                            "-------------------------------------",
+                        ].join("\n\n");
+                        console.log(concertData)
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+        if (fileCommand[0] === "movie-this") {
+            axios.get("https://www.omdbapi.com/?t=" + fileCommand[1] + "&y=&plot=short&apikey=trilogy")
+                .then(function (response) {
+                    var movieData = [
+                        "----------------------------------------",
+                        "Movie Title: " + response.data.Title,
+                        "Year of Release:" + response.data.Year,
+                        "IMDB Rating: " + response.data.imdbRating,
+                        "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value,
+                        "Country Produced: " + response.data.Country,
+                        "Language: " + response.data.Language,
+                        "Plot: " + response.data.Plot,
+                        "Actors: " + response.data.Actors,
+                        " ",
+                    ].join("\n\n");
+                    console.log(movieData);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        };
+    })
+
 };
